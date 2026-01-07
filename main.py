8c695099de3b381b77b543516d080684f2eacf76
Courtney <courtneglosser@gmail.com>
2026-01-07 13:26:43 -0500

    Adding basic player drawing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 
 from constants import *
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame_version = pygame.version.ver
@@ -11,13 +13,25 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0 # dt = "delta time"
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        # wait 1/{param} of a second, return # of ms
+        # divide by 1000 = delta time in seconds
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
