ea07dd59ac3fd5169e759f8eb650e1715ceed622
Courtney <courtneglosser@gmail.com>
2026-01-09 15:56:55 -0500

    Shots destroy asteroids

--- main.py
+++ main.py
@@ -52,6 +52,13 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
+            for s in shots:
+                if a.collides_with(s):
+                    log_event("asteroid_shot")
+                    a.kill()
+                    s.kill()
+
         for d in drawable:
             d.draw(screen)
 
