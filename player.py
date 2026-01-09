5afc5a34d84222e782c16cd3ae15c6e37d7489d6
Courtney <courtneglosser@gmail.com>
2026-01-09 15:48:18 -0500

    Introducing shot rate limiter

--- player.py
+++ player.py
@@ -12,6 +12,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -28,6 +29,10 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        # Manage shot timer:
+        self.shot_cooldown -= dt
+
+        # Handle key presses
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -49,8 +54,9 @@ class Player(CircleShape):
 
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity *= PLAYER_SHOOT_SPEED
-        
+        if self.shot_cooldown <= 0:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity *= PLAYER_SHOOT_SPEED
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 
